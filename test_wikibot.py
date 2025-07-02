f02082f62b896cdf7ec7132b9ccd4e185e283a7f
msmalmir <97928189+msmalmir@users.noreply.github.com>
2025-07-02 23:48:14 +0000

    assiding working code

--- test_wikibot.py
+++ test_wikibot.py
@@ -2,11 +2,12 @@ from mylib.bot import scrape
 from wikibot import cli
 from click.testing import CliRunner
 
+
 def test_wikibot():
     runner = CliRunner()
-    result = runner.invoke(cli, ['--name', 'Microsoft', '--length', '1'])
+    result = runner.invoke(cli, ["--name", "Microsoft", "--length", "1"])
     assert result.exit_code == 0
-    assert "Microsoft" in result.output 
+    assert "Microsoft" in result.output
 
 
 def test_scrape():
--- wikibot.py
+++ wikibot.py
@@ -2,19 +2,15 @@ from mylib.bot import scrape
 import click
 
 
-
 @click.command()
-@click.option('--name',    '-n',
-              default='Microsoft',
-              help='Wikipedia page to scrape')
-@click.option('--length',  '-l',
-              default=1,
-              type=int,
-              help='Number of sentences to return')
-
+@click.option("--name", "-n", default="Microsoft", help="Wikipedia page to scrape")
+@click.option(
+    "--length", "-l", default=1, type=int, help="Number of sentences to return"
+)
 def cli(name, length):
     result = scrape(name, length)
-    click.echo(click.style(f"{result}:", fg='white', bg='blue', bold=True))
+    click.echo(click.style(f"{result}:", fg="white", bg="blue", bold=True))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli()
